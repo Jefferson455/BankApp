20ee51589589250975b8c3d37008b1b1ab3098a8
jhonnier <jhonniercastro@gmail.com>
2025-04-21 14:58:39 -0500

    Se añadio la configuracion para firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:login_welcome/firebase_options.dart';
 // import 'package:login_welcome/src/screen/loginScreen.dart';
 import 'package:login_welcome/src/screen/welcome_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const LoginApp());
 }
 
